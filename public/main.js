ffffd8440d437237b1bf7dc8584534422827b5c7
George Nichols <georgehniii@gmail.com>
2022-05-25 12:18:34 -0700

    working back end and routes with .env

--- public/main.js
+++ public/main.js
@@ -1,11 +1,12 @@
+require("dotenv").config();
 const express = require('express');
 const req = require('express/lib/request');
 const app = express();
-const db = require('./queries')
-const port = process.env.Port || 5555;
+const db = require('./queries');
+const port = process.env.PORT || 5555;
 
 app.listen(port,function(){
-    console.log("Listing on 5555");
+    console.log(`Listing on port ${port}`);
 });
 
 app.use(express.json());
@@ -13,9 +14,9 @@ app.get('/items', db.getItems);
 app.get('/items/:id', db.getItemById);
 app.get('/category', db.getCategories);
 app.get('/category/:id',db.getItemsByCategory);
-//app.post('/items', db.createPet);
-//app.patch('/items/:id', db.updatePets);
-//app.delete('/items/:id', db.deletePet);
+//app.post('/items', db.createItem);
+//app.patch('/items/:id', db.updateItem);
+//app.delete('/items/:id', db.deleteItem);
 app.use((req,res) => {
     res.status(404).send("Not Found");
 });
--- public/queries.js
+++ public/queries.js
@@ -1,56 +1,73 @@
 //connecting to the database........
 const Pool = require('pg').Pool
+// const pool = new Pool({
+//   user: 'puser',
+//   host: 'localhost',
+//   database: 'mvp',
+//   password: 'asd123',
+//   port: 5432,
+// })
+
 const pool = new Pool({
-  user: 'puser',
-  host: 'localhost',
-  database: 'mvp',
-  password: 'asd123',
-  port: 5432,
-})
+    // Format: postgres://user:password@host:5432/database
+    
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false }
+  });
 
 
 const getItems = async (req, res) => {
+    console.log("fetching items");
     try{
-    await pool.query('SELECT * FROM items', (err, results) => {
-      res.status(200).json(results.rows)
-    })
-     } catch (error){
-          console.err(err.message)
-      }
+        const data = await pool.query('SELECT * FROM items');
+        res.send(data.rows);
+    }
+    catch (error){
+        console.err(error);
+    }
    
   }
 
-const getItemById = (req, res) => {
-    const id = req.params.id
-    pool.query('SELECT * FROM items WHERE item_id = $1', [id], (err, results) => {
-        if (err) {
-            throw err
-        }
-        res.status(200).json(results.rows)
-    })
+const getItemById = async (req, res) => {
+    const id = req.params.id;
+    console.log("fetching item by id");
+    try{
+        const data = await pool.query('SELECT * FROM items WHERE item_id = $1',[id]);
+        res.send(data.rows);
+        res.status(200);
+    }
+    catch (error){
+        console.log(error);
+    }
 }
 
 const getCategories = async (req, res) => {
+    console.log("fetching categories");
     try{
-    await pool.query('SELECT * FROM categories', (err, results) => {
-      res.status(200).json(results.rows)
-    })
-     } catch (error){
-          console.err(err.message)
-      }
-   
+        const data = await pool.query('SELECT * FROM categories');
+        res.send(data.rows);
+        res.status(200);
+    } 
+    catch (error){
+        console.err(error.message);
+    } 
   }
 
-const getItemsByCategory = (req, res) => {
-    const id = req.params.id
-    pool.query('SELECT * FROM items WHERE category_id = $1', [id], (err, results) => {
-        if (err) {
-            throw err
-        }
-        res.status(200).json(results.rows)
-    })
+const getItemsByCategory = async (req, res) => {
+    console.log("fetching item by category");
+    const id = req.params.id;
+    try{
+        const data = await pool.query('SELECT * FROM items WHERE category_id = $1',[id]);
+        res.send(data.rows);
+        console.log(data.rows);
+        res.status(200);
+    }
+    catch (error){
+        console.log(error);
+    }
 }
 /*
+//have to fix the ry cathces
 const createItem = (req, res) => {
     const {category_id,item_name,price} = req.body
 
