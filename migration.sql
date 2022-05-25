92433958eac9ffc0ecd0c038e48a1f59baaf9832
George Nichols <georgehniii@gmail.com>
2022-05-24 17:06:42 -0700

    This is being a pain

--- migration.sql
+++ migration.sql
@@ -1,4 +1,4 @@
 DROP TABLE IF EXISTS items;
 DROP TABLE IF EXISTS categories;
 CREATE TABLE categories (category_id INT PRIMARY KEY, category TEXT);
-CREATE TABLE items (item_id SERIAL PRIMARY KEY, category_id refrences catergories(category_id), item_name TEXT, price FLOAT);
+CREATE TABLE items (item_id SERIAL PRIMARY KEY, category_id INT refErences categories(category_id), item_name TEXT, price FLOAT);
