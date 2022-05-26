60bee751dc2e5ff09dd1e382bc61730aacf167c3
George Nichols <georgehniii@gmail.com>
2022-05-25 21:49:18 -0700

    working localy

--- public/main.js
+++ public/main.js
@@ -1,22 +1,80 @@
-require("dotenv").config();
-const express = require('express');
-const req = require('express/lib/request');
-const app = express();
-const db = require('./queries');
-const port = process.env.PORT || 5555;
+//const { client_encoding } = require("pg/lib/defaults");
 
-app.listen(port,function(){
-    console.log(`Listing on port ${port}`);
+let $homeButton = $("#home");
+let $allButton = $("#all");
+let $inputContainer = $("#inputContainer");
+const start = async () => {
+    try{
+        const data = await $.get('http://localhost:5555/category');
+        console.log(data);
+        pageLoader(data,"category");
+    }
+    catch (error){
+        console.err(error);
+    }
+    
+}
+start();
+$homeButton.on("click", start);
+
+$allButton.on("click", async () => {
+    try{
+        const data = await $.get('http://localhost:5555/items');
+        console.log(data);
+        pageLoader(data,"item");
+    }
+    catch (error){
+        console.err(error);
+    }
 });
 
-app.use(express.json());
-app.get('/items', db.getItems);
-app.get('/items/:id', db.getItemById);
-app.get('/category', db.getCategories);
-app.get('/category/:id',db.getItemsByCategory);
-//app.post('/items', db.createItem);
-//app.patch('/items/:id', db.updateItem);
-//app.delete('/items/:id', db.deleteItem);
-app.use((req,res) => {
-    res.status(404).send("Not Found");
-});
+function pageLoader(data,opt){
+    $(".inputs").remove();
+    console.log(data);
+    for(var i = 0; i < data.length; i++){
+        const $infoBox = $(`<div  class='inputs ${JSON.stringify(data[i])}'></div>`);
+        $infoBox.click(buttonBuilder);
+        $infoBox.text(data[i][opt]);
+        $infoBox.appendTo($inputContainer);
+    }
+}
+
+function pageLoader2(data){
+    $(".inputs").remove();
+    console.log(data);
+    for(var i = 0; i < data.length; i++){
+        const $infoBox = $(`<div  class='inputs'></div>`);
+        $infoBox.text(`${data[i]["item"]} \nPrice: $${data[i]["price"]}`);
+        $infoBox.appendTo($inputContainer);
+    }
+}
+
+async function buttonBuilder(e){
+    console.log(`building button`);
+    console.log(`clicked ${this.textContent}`);
+    console.log(`clicked ${this.className}`);
+    const dirtyString = this.className;
+    const cleanedObj = dirtyString.slice(7,dirtyString.length)
+    const obj = JSON.parse(cleanedObj);
+    console.log(obj);
+    let opt = "";
+    if(obj.item_id != undefined){
+        opt = "item";
+    }else{
+        opt = "category";
+    }
+    console.log(opt);
+    try{
+        const data = await $.get(`http://localhost:5555/${opt}/${obj[opt+"_id"]}`);
+        console.log("You clicked it");
+        if(opt == "category"){
+            pageLoader(data,"item");
+        }
+        else{
+            pageLoader2(data);
+        }
+    }
+    catch (error){
+        console.err(error);
+    }
+}
